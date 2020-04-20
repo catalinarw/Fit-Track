757f1e37e25782cf7760521545f1d134cb738c48
Catalina Williams <catalina.rose1361@gmail.com>
2020-04-20 02:38:25 -0600

    changes to mongo uri

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Connecting to either our remote Mongo db or our local db
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://username:password@ds031751.mlab.com:31751/heroku_jnn204qw", { useNewUrlParser: true });
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -29,44 +29,7 @@ app.use(express.static("public"));
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
-// app.get("/", function(req, res) {
-//     res.sendFile(path.join(__dirname, "index.html"))
-// })
 
-// app.get("/exercise", function(req, res) {
-//     res.sendFile(path.join(__dirname, "./public/exercise.html" ))
-// })
-
-// app.get("/stats", function(req, res) {
-//     res.sendFile(path.join(__dirname, "./public/stats.html"))
-// })
-
-// app.post("/api/workouts", function(req, res) {
-//     db.find().then(data => {
-//         console.log(data)
-//         res.json(data)
-//     })
-// })
-
-// app.get("/api/workouts", function(req, res) {
-//     db.find().then(results => {
-//         res.json(results[results.length - 1])
-//     })
-// })
-
-// app.get("/api/workouts/range", function(res, req) {
-//     db.find().then(data => {
-//         res.json(data)
-//     })
-// })
-
-// app.put("/api/workouts/:id", function(req, res) {
-//     console.log(req.body)
-//     db.update(
-//         { _id: req.params.id },
-//         { $push: { exercises: req.body } }
-//     ).then(response => res.json(response))
-// })
 // Starting our Express app
 // =============================================================
 app.listen(PORT, () => {
