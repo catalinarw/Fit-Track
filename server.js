f506bbbeaacc8095c8e5af6842520e1914f455b9
Catalina Williams <catalina.rose1361@gmail.com>
2020-04-21 03:35:50 -0600

    h

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-var dotenv = require('dotenv')
-dotenv.config()
+
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 // const path = require("path")
 // const db = require("./models/workout")
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
 const app = express();
 
 
